740c44f9d87bc42ef28db94b7f82b1810dd6e3e5
Dominic <dominicacox@gmail.com>
2015-08-16 12:36:44 -0400

    Added in basic notification checking. Routing broken on all pages. Testing still needs to happen.

--- Sigil/Controllers/IssueController.cs
+++ Sigil/Controllers/IssueController.cs
@@ -73,6 +73,7 @@ namespace Sigil.Controllers
                     newComment.votes = 1;
 
                     newComment.text = Request.Form["text"];
+                    Notification_Check(newComment.text, userID);
                     
                     // Try to submit the issue and go to the issue page; otherwise, write an error
                     try
@@ -509,6 +510,47 @@ namespace Sigil.Controllers
                 Console.WriteLine( "Could not unvote on issue %s:\n%s", thisIssue.Id, e.Message );
             }
             return new EmptyResult();
+        }
+
+        private void Notification_Check(string text, string user)
+        {
+            string to_user = null;
+            string to_org = null;
+            var text_words = text.Split(' ');
+            foreach (string t in text_words)
+            {
+                if (t[0] == '@')
+                {
+                    to_user = t.Remove(0, 1);
+                    
+                }
+
+            }
+
+            if (to_user != null || to_org != null)
+            {
+                var to_user_id = from users in dc.AspNetUsers
+                                 where users.UserName == to_user
+                                 select users.Id;
+
+                var to_org_id = from org in dc.Orgs
+                                where org.orgName == to_org
+                                select org.Id;
+
+                Notification note = new Notification();
+                try
+                {
+                    note.From_UserId = user;
+                    note.To_UserId = to_user_id.ToString();
+                    note.To_OrgId = Convert.ToInt32(to_org_id.ToString());
+                    dc.Notifications.InsertOnSubmit(note);
+                    dc.SubmitChanges();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Could not unvote on issue %s:\n%s", note.Id, e.Message);
+                }
+            }
         }
     }
 }
