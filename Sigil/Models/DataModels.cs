2a1a0bd2bd588bddd1371298786e79d8e53ad946
Nerve <nervecenter@outlook.com>
2015-10-07 09:21:23 -0700

    Landing page working. Yay.

--- Sigil/Models/DataModels.cs
+++ Sigil/Models/DataModels.cs
@@ -11,6 +11,13 @@ using System.Data.SqlTypes;
 using System.Xml.Linq;
 namespace Sigil.Models
 {
+    public struct IssuePanelOptions {
+        public bool inPanel;
+        public bool showOrg;
+        public bool showCat;
+        public bool showTopic;
+        public bool userVoted;
+    }
 
     public static class CountXML<T>
     {
