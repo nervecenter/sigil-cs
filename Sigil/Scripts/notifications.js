4cdd25354282574d51e22a58eb016eda104abff0
Nerve <nervecenter@outlook.com>
2016-01-28 22:34:19 -0500

    Navbar working. Fixed search page formatting slightly. Deleted old notifications.js code.

--- Sigil/Scripts/notifications.js
+++ Sigil/Scripts/notifications.js
@@ -1,53 +1,4 @@
-﻿/*function shownotifications() {
-    var icon = document.getElementById("header-user-icon");
-    icon.onclick = function () { hidenotifications() }
-    
-    var shade = document.createElement("div");
-    shade.id = "notifications-shade";
-    shade.innerHTML = "<img class='callout' src='/Content/Images/callout.png' /><div class='panel panel-default notifications-panel'><div id='NoteParent' class='panel-body'></div></div>";
-    document.getElementById("navbar-list").appendChild(shade);
-    
-    
-    $.ajax({
-        url: "/check_notes",
-        success: function (data) {
-            $.each(data, function (index, Note) {
-                var media = document.createElement("div"); //parent div for entire notification
-                media.classList.add("media");
-                var imganchor = document.createElement("a"); //icon for from user -- need to change return value of from userid to a link to their icon instead
-                imganchor.classList.add('pull-left');
-                var img = document.createElement("img"); //img container for the user icon
-                img.setAttribute("src", '/Images/User/nervecenter_100.png');
-                img.classList.add('media-object');
-                img.classList.add('notification-icon');
-                imganchor.appendChild(img);
-                var mediabody = document.createElement("div"); //div container that includes the title and link
-                mediabody.classList.add('media-body');
-                var notificationtext = document.createElement("a"); //link container that allows clicking the notification to take you to where it is
-                notificationtext.setAttribute("href", Note.url);
-                //mediabody.setAttribute('href', Note.url);
-                //var mediabodySpan = document.createElement("span"); //The span that includes the issue title where the notification took place
-                notificationtext.innerHTML = Note.title;
-                mediabody.appendChild(notificationtext);
-                //nURL.appendChild(mediabodySpan);
-                media.appendChild(imganchor);
-                media.appendChild(mediabody);
-                document.getElementById("NoteParent").appendChild(media); 
-            });
-            //should probably include a check on data to see if there is stuff there and if not just have a message that shows no notifications
-        }
-    });
-       
-}*/
-
-/*function hidenotifications() {
-    var icon = document.getElementById("header-user-icon");
-    icon.onclick = function () { shownotifications() }
-
-    document.getElementById("notifications-shade").remove();
-}*/
-
-function shownotifications() {
+﻿function shownotifications() {
     $("#header-user-icon").off("click").click(hidenotifications);
 
     //<div id='NoteParent' class='panel-body'></div>
@@ -72,7 +23,7 @@ function shownotifications() {
         .append($callout)
         .append($panel);
 
-    $("#navbar-list").append($shade);
+    $("#navbar-header").append($shade);
 
     $.get("/check_notes",
         function (data) {
